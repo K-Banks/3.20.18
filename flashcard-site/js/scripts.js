20826d2b3b134f579f5e1e5cdcf9f33490cd5442
Cory West-Forbes and Kayl Eubanks <cory@>
2018-03-20 11:07:29 -0700

    finalize functionality of the flashcards page

--- flashcard-site/js/scripts.js
+++ flashcard-site/js/scripts.js
@@ -1,22 +1,34 @@
 $(document).ready(function() {
 
-  $("h1").click(function() {
-    $("h2").slideToggle("slow");
+  var arrayTerm = ['JavaScript', 'Operators', 'Variables', "Functions", "methods"];
+
+  var arrayDefinition = ['definition 1', 'definition 2', 'definition 3', 'definition 4', 'definition 5'];
+
+  function newCard() {
+    var arrayGen = Math.floor(Math.random() *  4);
+    var newTerm = arrayTerm[arrayGen];
+    var newDefinition = arrayDefinition[arrayGen];
+
+    $('.panel-title').fadeOut(200, function() {
+      $(this).html(newTerm).fadeIn(200);
+      $("panel-body").addClass("hide");
+    });
+
+    $('.panel-body').fadeOut(200, function() {
+      $(this).html(newDefinition);
+    });
+  }
+
+  $('.panel-title').click(function() {
+    $('.panel-body').removeClass("hide");
+    $(".panel-body").addClass("show");
+    $('button').removeClass("hide");
   });
 
-  $("p").click(function() {
-    $("h1").fadeToggle("fast");
-  });
-
-  $(".button-cat").click(function() {
-    $("img").toggle();
-  });
-
-  $(".button-dark").click(function(){
-    $("body").toggleClass("dark");
-  });
-
-  $("p").click(function() {
-    $("p").toggleClass("p-box");
-  });
+    $("button").click(function() {
+      newCard();
+      $('.panel-body').removeClass("show");
+      $(".panel-body").addClass("hide");
+      $('button').addClass("hide");
+    });
 });
