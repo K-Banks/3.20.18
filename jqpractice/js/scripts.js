d0047fde53d9f2a9ad9eb26619678ffb585b4e03
Cory West-Forbes and Kayl Eubanks <kayl@>
2018-03-20 09:44:07 -0700

    hide images in css, create button that changes the background color to black with white text and changes back to default styling

--- jqpractice/js/scripts.js
+++ jqpractice/js/scripts.js
@@ -8,7 +8,15 @@ $(document).ready(function() {
     $("h1").fadeToggle("fast");
   });
 
-  $("button").click(function() {
+  $(".button-cat").click(function() {
     $("img").toggle();
+  });
+
+  $(".button-dark").click(function(){
+    $("body").toggleClass("dark");
+  });
+
+  $("p").click(function() {
+    $("p").toggleClass("p-box");
   });
 });
